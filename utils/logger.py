1dc6f230d320f8567ae8dcdb7140cb9a5d83a51b
innTall <avg999.trade@gmail.com>
2025-09-05 18:56:58 +0200

    New fractal bot first commit

--- utils/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def setup_logger(level="INFO"):
-    logging.basicConfig(
-        level=getattr(logging, level.upper(), logging.INFO),
-        format="[%(levelname)s] %(message)s"
-    )
-    return logging.getLogger("smc")
